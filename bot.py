ed75c89394415084f57cce4f4666f2ab1067f4c6
bananaboy21 <33041238+bananaboy21@users.noreply.github.com>
2018-04-15 12:39:30 -0400

    why prefix no work?

--- bot.py
+++ bot.py
@@ -22,6 +22,8 @@ db = AsyncIOMotorClient(os.environ.get("MONGODB"))
 
 async def getprefix(bot, message):
     x = await db.datbananabot.prefix.find_one({"id": str(message.guild.id)})
+    if x is None:
+        return "="
     pre = x.get('prefix', "=")
     return pre 
 
